37b06a02fec00664ddab36b8c3285d6244787256
NikeNano <niklas.sven.hansson@gmail.com>
2022-03-06 16:22:23 +0100

    feat: slack integration working

--- src/go/main.go
+++ src/go/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println("body: ", body)
 	resp := make(map[string]map[string]string)
 	resp["node"] = map[string]string{}
 	resp["node"]["phase"] = "Succeeded"
@@ -15,6 +24,7 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 	}
+	time.Sleep(time.Minute * 10)
 	w.Write(jsonResp)
 }
 
